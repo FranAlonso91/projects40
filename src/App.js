c1392601b34401ea3d1ec0f496e2071a48e65870
Fran Alonso <franciscojalonso91@gmail.com>
2022-12-13 12:09:52 +0100

    calculator styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand"></div>
+        <div className="current-operand"></div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
@@ -12,6 +13,7 @@ function App() {
       <button>1</button>
       <button>2</button>
       <button>3</button>
+      <button>*</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
